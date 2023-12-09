190fa48a2148e3c904596e82ae98efa655ab8810
Roman <rubin3215@gmail.com>
2023-12-09 16:02:13 +0300

    создал модели2

--- core/models.py
+++ core/models.py
@@ -2,23 +2,35 @@ from django.db import models
 
 
 class Creator(models.Model):
-    nick = models.CharField('имя', max_length=100)
+    nickname = models.CharField('имя', max_length=100)
     dr = models.DateTimeField('дата регистриции', auto_now_add=True)
 
     def __str__(self):
-        return self.nick
+        return self.nickname
 
 
 class Poll(models.Model):
-    creator = models.ForeignKey(Creator, verbose_name='creator', on_delete=models.CASCADE, related_name='опросы')
+    creator = models.ForeignKey(Creator, verbose_name='creator', on_delete=models.CASCADE, related_name='polls')
     theme = models.CharField('название', max_length=100)
-    answer1 = models.CharField('ответ1', max_length=100)
-    answer2 = models.CharField('ответ2', max_length=100)
-    answer3 = models.CharField('ответ3', max_length=100)
-    res1 = models.IntegerField('результат1', default=0)
-    res2 = models.IntegerField('результат2', default=0)
-    res3 = models.IntegerField('результат3', default=0)
     dc = models.DateTimeField('дата создания', auto_now_add=True)
 
     def __str__(self):
-        return f'Опрос "{self.theme}"'
+        return self.theme
+
+
+class Question(models.Model):
+    poll = models.ForeignKey(Poll, verbose_name='poll', on_delete=models.CASCADE, related_name='questions')
+    question = models.CharField('вопрос', max_length=100)
+
+    def __str__(self):
+        return self.question
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, verbose_name='question', on_delete=models.CASCADE, related_name='answers')
+    answer = models.CharField('ответ', max_length=100)
+    is_correct = models.BooleanField('выбран', default=False)
+    count = models.IntegerField('счётчик', default=0)
+
+    def __str__(self):
+        return self.answer
