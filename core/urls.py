e58e0928bd2fd8e2c4f74fd57053697887572a6a
Roman <rubin3215@gmail.com>
2023-12-10 23:35:20 +0300

    создал классы представление, шаблоны

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from core.views import Homepage
-from core.views import Info
+from core.views import Mainpage, Info, Creators, Creator, Polls, Poll
 
 
 urlpatterns = [
-    path('', Homepage.as_view(), name='homepage'),
+    path('', Mainpage.as_view(), name='mainpage'),
     path('info/', Info.as_view(), name='info'),
+    path('creators/', Creators.as_view(), name='creators'),
+    path('creator/<id>', Creator.as_view(), name='creator'),
+    path('polls/', Polls.as_view(), name='polls'),
+    path('poll/<id>', Poll.as_view(), name='poll'),
 ]
